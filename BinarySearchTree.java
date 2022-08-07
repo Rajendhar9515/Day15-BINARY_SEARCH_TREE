1a97eb74795d49df1e1496e544741d55a86fc7ed
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-08-07 19:18:13 +0530

    Checked if all are added using size method.

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -11,13 +11,13 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	// Assigning data in Binary search tree format.
 	public MyBinaryNode<T> insertElementRecursive(MyBinaryNode<T> root, T data) {
 		if (root == null) {
-			root = new MyBinaryNode(data);
+			root = new MyBinaryNode<T>(data);
 			return root;
 		}
- 
-		if (data.compareTo(root.data) < 0) {                  // if data is less than the root , then data will assign to the left side of the root.
+
+		if (data.compareTo(root.data) < 0) {                        // if data is less than the root , then data will assign to the left side of the root.
 			root.left = insertElementRecursive(root.left, data);
-		} else if (data.compareTo(root.data) > 0) {           // if data is greater than the root , then data will assign to the right side of the root.
+		} else if (data.compareTo(root.data) > 0) {                 // if data is greater than the root , then data will assign to the right side of the root.
 			root.right = insertElementRecursive(root.right, data);
 		}
 		return root;
@@ -53,6 +53,17 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		System.out.print(node.data + "  ");
 	}
 
+	// Size of the BST
+	public int size() {
+		return this.getSizeRecursive(root);
+	}
+
+	public int getSizeRecursive(MyBinaryNode<T> root) {
+
+		// Ternary Operator
+		return (root == null) ? 0 : this.getSizeRecursive(root.left) + 1 + this.getSizeRecursive(root.right);
+	}
+
 	// Main Method
 	public static void main(String[] args) {
 		System.out.println("\t WELCOME TO BINARY-SEARCH-TREE \n");
@@ -63,6 +74,16 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		bst.add(56);
 		bst.add(30);
 		bst.add(70);
+		bst.add(22);
+		bst.add(40);
+		bst.add(11);
+		bst.add(3);
+		bst.add(16);
+		bst.add(60);
+		bst.add(95);
+		bst.add(63);
+		bst.add(65);
+		bst.add(67);
 
 		System.out.print("Pre-Order Traversal:- ");
 		bst.preOrderTraversal(root);
@@ -73,5 +94,6 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		System.out.print("\n\nPost-Order Traversal:- ");
 		bst.postOrderTraversal(root);
 
+		System.out.println("\n\nSize of the Binary Search Tree :- " + bst.size());
 	}
 }
