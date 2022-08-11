a9a61109d9d262ee08cf96fe03e6be954054c7e2
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-08-11 10:43:34 +0530

    checking frequency of words for removing avoidable word.

--- HashTable.java
+++ HashTable.java
@@ -1,55 +1,84 @@
-package com.binary.search.tree;
+package com.bridgelabz.hash_table;
 
 class HashTable<K, V> {
 
-	MyMapNode<K, V> head;
-	MyMapNode<K, V> tail;
+    MyMapNode<K, V> head;
+    MyMapNode<K, V> tail;
 
-	public void add(K key, V value) {
-		MyMapNode<K, V> myNode = (MyMapNode<K, V>) searchNode(key);
-		if (myNode == null) {
-			myNode = new MyMapNode<>(key, value);
-			this.append(myNode);
-		} else {
-			myNode.setValue(value);
-		}
-	}
+    public void add(K key, V value) {
+        MyMapNode<K, V> myNode = (MyMapNode<K, V>) searchNode(key);
+        if(myNode == null) {
+            myNode = new MyMapNode<>(key , value);
+            this.append(myNode);
+        }
+        else {
+            myNode.setValue(value);
+        }
+    }
 
-	public void append(MyMapNode<K, V> myNode) {
-		if (this.head == null)
-			this.head = myNode;
-		if (this.tail == null)
-			this.tail = myNode;
-		else {
-			this.tail.setNext(myNode);
-			this.tail = myNode;
-		}
-	}
+    public void append(MyMapNode<K, V> myNode) {
+        if(this.head == null)
+            this.head = myNode;
+        if(this.tail == null)
+            this.tail = myNode;
+        else {
+            this.tail.setNext(myNode);
+            this.tail = myNode;
+        }
+    }
 
-	public MyMapNode<K, V> searchNode(K data) {
-		MyMapNode<K, V> currentNode = head;
-		int position = 0;
-		while (currentNode != null) {
-			position++;
-			if (currentNode.getKey().equals(data)) {
-				return currentNode;
-			}
-			currentNode = currentNode.getNext();
-		}
-		return currentNode;
-	}
+    public MyMapNode<K, V> searchNode(K data) {
+        MyMapNode<K, V> currentNode = head;
+        int position = 0;
+        while (currentNode != null) {
+            position++;
+            if (currentNode.getKey().equals(data)) {
+                return currentNode;
+            }
+            currentNode = currentNode.getNext();
+        }
+        return currentNode;
+    }
 
-	public V get(K word) {
-		MyMapNode<K, V> myMapNode = searchNode(word);
-		return (myMapNode == null) ? null : myMapNode.getValue();
-	}
+    public V get(K word) {
+        MyMapNode<K, V> myMapNode = searchNode(word);
+        return (myMapNode == null) ? null : myMapNode.getValue();
+    }
 
-	@Override
-	public String toString() {
-		return "Linked List Nodes { " + head + " }";
-	}
+    public void remove(K word) {
+        MyMapNode<K, V> currentNode = head;
+        MyMapNode<K, V> previousNode = null;
+        while (currentNode != null && currentNode.getKey().equals(word)) {
+            head = currentNode.getNext();
+            return;
+        }
+        while (currentNode != null && !(currentNode.getKey().equals(word))) {
+            previousNode = currentNode;
+            currentNode = currentNode.getNext();
+        }
+        if (currentNode != null) {
+            previousNode.next = currentNode.next;
+        }
+        if(currentNode == null)
+            System.out.println("Word not found!");
+    }
 
-	public void printNodes() {
-		System.out.println("My nodes: " + head);
-	}
+    public int size(){
+        MyMapNode<K, V> currentNode = head;
+        int position = 0;
+        while (currentNode != null) {
+            position++;
+            currentNode = currentNode.getNext();
+        }
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return "Linked List Nodes { " + head + " }";
+    }
+
+    public void printNodes() {
+        System.out.println("My nodes: " + head);
+    }
 }
--- Main.java
+++ Main.java
@@ -1,24 +1,25 @@
-package com.binary.search.tree;
+package com.bridgelabz.hash_table;
 
 public class Main {
 
 	public static void main(String[] args) {
-		HashTable<String, Integer> hashImpl = new HashTable<>();
+		HashTable<String, Integer> map = new HashTable<>();
 		String message = "Paranoids are not paranoid because they are paranoid but "
 				+ "because they keep putting themselves deliberately into paranoid avoidable situations";
 
 		String[] messageArray = message.toLowerCase().split(" ");
 
 		for (String word : messageArray) {
-			Integer value = hashImpl.get(word);
+			Integer value = map.get(word);
 
-			if (value == null) {
+			if (value == null)
 				value = 1;
-			} else {
+			else
 				value = value + 1;
-			}
-			hashImpl.add(word, value);
+			map.add(word, value);
 		}
-		System.out.println(hashImpl);
+		System.out.println(map.size());
+		map.remove("avoidable");
+		System.out.println(map.size());
 	}
 }
